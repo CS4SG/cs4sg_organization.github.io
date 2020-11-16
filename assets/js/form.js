4c44485bd6381abd9265e77454a2d0cf889528fd
Prakshal <prakshal@buffalo.edu>
2020-11-16 12:09:50 -0500

    Added Multiple Resources

---
+++ assets/js/form.js
@@ -0,0 +1,21 @@
+function collapse(id){
+    var coll = document.getElementsByClassName(id);
+    var i;
+  
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        console.log(this.nextElementSibling)
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+}
+
+collapse("collapsible_slides")
+collapse("collapsible_links")
+collapse("collapsible_papers")
