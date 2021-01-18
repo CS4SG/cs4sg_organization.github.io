5e2aec4a0c5ccb6d2256100f74ee4e3591847601
Prakshal Jain <61107289+Prakshal-Jain@users.noreply.github.com>
2021-01-18 16:14:19 -0500

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,22 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+function collapse(id){
+    var coll = document.getElementsByClassName(id);
+    var i;
+  
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
 }
+
+collapse("collapsible_slides")
+collapse("collapsible_links")
 
 
 
